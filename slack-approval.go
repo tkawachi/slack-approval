bae40d25fca90329b3fc00db1f90470604023d6d
Takashi Kawachi <tkawachi@gmail.com>
2021-04-06 23:03:49 +0900

    update

--- slack-approval.go
+++ slack-approval.go
@@ -22,15 +22,21 @@ func mustGetEnv(key string) string {
 	return value
 }
 
+func postApproveMessage(webAPI *slack.Client, channelID, message string) (string, error) {
+	// TODO
+}
+
 func main() {
 	getopt.FlagLong(&verbose, "verbose", 'v', "verbose output")
 	helpFlag := getopt.BoolLong("help", 'h', "show help")
+	channelIDFlag := getopt.StringLong("channel", 'c', "Channel ID to posting an approve message")
 	getopt.SetParameters("message")
 	getopt.Parse()
 	if *helpFlag {
 		getopt.Usage()
 		os.Exit(1)
 	}
+	message := strings.Join(getopt.Args(), " ")
 	webAPI := slack.New(
 		mustGetEnv("SLACK_BOT_TOKEN"),
 		slack.OptionAppLevelToken(mustGetEnv("SLACK_APP_TOKEN")),
@@ -40,7 +46,7 @@ func main() {
 	socketMode := socketmode.New(
 		webAPI,
 		socketmode.OptionDebug(verbose),
-		socketmode.OptionLog(log.New(os.Stdout, "sm: ", log.Lshortfile|log.LstdFlags)),
+		socketmode.OptionLog(log.New(os.Stderr, "sm: ", log.Lshortfile|log.LstdFlags)),
 	)
 	authTest, authTestErr := webAPI.AuthTest()
 	if authTestErr != nil {
@@ -48,7 +54,11 @@ func main() {
 		os.Exit(1)
 	}
 	selfUserID := authTest.UserID
-
+	buttonID, err := postApproveMessage(webAPI, *channelIDFlag, message)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("TODO:", buttonID)
 	go func() {
 		for envelope := range socketMode.Events {
 			switch envelope.Type {
@@ -82,69 +92,9 @@ func main() {
 					socketMode.Debugf("unsupported Events API eventPayload received")
 				}
 			case socketmode.EventTypeInteractive:
-				// ショートカットのハンドリングとモーダル起動
 				payload, _ := envelope.Data.(slack.InteractionCallback)
 				switch payload.Type {
-				case slack.InteractionTypeShortcut:
-					if payload.CallbackID == "socket-mode-shortcut" {
-						socketMode.Ack(*envelope.Request)
-						modalView := slack.ModalViewRequest{
-							Type:       "modal",
-							CallbackID: "modal-id",
-							Title: slack.NewTextBlockObject(
-								"plain_text",
-								"タスク登録",
-								false,
-								false,
-							),
-							Submit: slack.NewTextBlockObject(
-								"plain_text",
-								"送信",
-								false,
-								false,
-							),
-							Close: slack.NewTextBlockObject(
-								"plain_text",
-								"キャンセル",
-								false,
-								false,
-							),
-							Blocks: slack.Blocks{
-								BlockSet: []slack.Block{
-									slack.NewInputBlock(
-										"input-task",
-										slack.NewTextBlockObject(
-											"plain_text",
-											"タスク",
-											false,
-											false,
-										),
-										// multiline is not yet supported
-										slack.NewPlainTextInputBlockElement(
-											slack.NewTextBlockObject(
-												"plain_text",
-												"タスクの詳細・期限などを書いてください",
-												false,
-												false,
-											),
-											"input",
-										),
-									),
-								},
-							},
-						}
-						resp, err := webAPI.OpenView(payload.TriggerID, modalView)
-						if err != nil {
-							log.Printf("Failed to opemn a modal: %v", err)
-						}
-						socketMode.Debugf("views.open response: %v", resp)
-					}
-				case slack.InteractionTypeViewSubmission:
-					// モーダルからの送信をハンドリング
-					if payload.CallbackID == "modal-id" {
-						socketMode.Debugf("Submitted Data: %v", payload.View.State.Values)
-						socketMode.Ack(*envelope.Request)
-					}
+				// TODO
 				default:
 					socketMode.Debugf("Skipped: %v", payload)
 				}
